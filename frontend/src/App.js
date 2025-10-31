233dd118e17e4b113acc4e25ae8741a59b857241
PURNENDU2004-crypto <purnendu.jana2004@gmail.com>
2025-10-31 11:27:27 +0530

    Added City and Recharge Dropdown components

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CityDropdown from "./components/CityDropdown";
+import ISP_Provider from "./components/ISP_Provider";
+
+
+function App() {
+  return (
+    <div>
+      
+      <CityDropdown />
+      <ISP_Provider/>
+    </div>
+  );
+}
+
+export default App;
